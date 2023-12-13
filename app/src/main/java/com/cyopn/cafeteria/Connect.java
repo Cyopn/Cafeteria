7ca6d2108b7d35d8fd1ed252c2ca4350f9b24ef9
Cyopn <calvillobenitezjuanantonio@gmail.com>
2023-12-12 23:58:31 -0600

    nose

--- app/src/main/java/com/cyopn/cafeteria/Connect.java
+++ app/src/main/java/com/cyopn/cafeteria/Connect.java
@@ -1,18 +1,57 @@
 package com.cyopn.cafeteria;
-import java.sql.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Connect {
     Connection conn = null;
-    String ip="192.168.253.176";
+    Statement st = null;
+    String ip = "192.168.253.176";
+    List<String> ips = new ArrayList<String>();
 
-public Connect(){
-    try {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        this.conn = DriverManager.getConnection("jdbc:mysql://" + ip + "/?useSSL=FALSE&allowPublicKeyRetrieval=TRUE", "userapp", "caW4630yhs19..");
-    } catch (ClassNotFoundException | SQLException e) {
-        System.out.println(e);
+    public Connect() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            this.conn = DriverManager.getConnection("jdbc:postgresql:" + ip + ":5432/test", "cyopn", "huevo123");
+            st = this.conn.createStatement();
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e);
+        }
     }
-}
+
     public Connection getConnect() {
         return this.conn;
     }
+
+    public void execSelect(String query) {
+        Map<String, List<String>> result = new HashMap<>();
+        try {
+            ResultSet rs = st.executeQuery("SELECT * FROM public.\"" + query + "\"");
+            ArrayList<String> rsl = new ArrayList<>();
+            if (rs.next()) {
+                rsl.add("sucess");
+                result.put("status", rsl);
+                rsl.remove(0);
+                while (rs.next()) {
+                    rs.getString("");
+                    rsl.add(rs.getString("name"));
+                    rsl.add(rs.getString("lastname"));
+                    rsl.add(rs.getString("role"));
+                    result.put(rs.getString("register"), rsl);
+                }
+            }
+        } catch (SQLException e) {
+
+        }
+
+
+    }
+
 }
--- app/src/main/java/com/cyopn/cafeteria/MainActivity.java
+++ app/src/main/java/com/cyopn/cafeteria/MainActivity.java
@@ -18,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         cn = c.getConnect();
         setContentView(R.layout.activity_main);
-
     }
 
     public void onClicContine(View v) {
